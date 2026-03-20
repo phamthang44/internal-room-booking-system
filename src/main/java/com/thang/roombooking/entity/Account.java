4426c38f6e0d1878d354207a6177b6a9fd1627b6
phamthang44 <Ducthang4342@gmail.com>
2026-03-20 09:54:25 +0700

    initialize project

---
+++ src/main/java/com/thang/roombooking/entity/Account.java
@@ -0,0 +1,4 @@
+package com.thang.roombooking.entity;
+
+public class Account {
+}
---
+++ src/test/java/com/thang/roombooking/EquipmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thang.roombooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
